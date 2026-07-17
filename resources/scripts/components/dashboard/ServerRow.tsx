b7b4f96b23094e1a6f8cd3f3a5094dcb31c9d050
Jack H <jackh.224307@gmail.com>
2026-07-16 19:07:20 -0500

    Update ServerRow.tsx

--- resources/scripts/components/dashboard/ServerRow.tsx
+++ resources/scripts/components/dashboard/ServerRow.tsx
@@ -108,7 +108,7 @@ export default ({ server, className, draggable, isDragging, onDragStart, onDragE
                     }}
                     onDragEnd={() => onDragEnd?.()}
                     title={'Drag to reorder'}
-                    css={tw`flex items-center px-3 mr-2 rounded cursor-grab active:cursor-grabbing bg-neutral-700 text-neutral-300 hover:text-neutral-100`}
+                    css={tw`flex items-center px-3 mr-2 rounded cursor-move bg-neutral-700 text-neutral-300 hover:text-neutral-100`}
                 >
                     <FontAwesomeIcon icon={faBars} />
                 </div>
